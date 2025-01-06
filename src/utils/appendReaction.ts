9eaba6a28d5785c33fb41b9c0be66b0ff3e581b4
eirikhanasand <eirik.hanasand@gmail.com>
2025-01-06 03:08:23 +0100

    Fixed bug where the reaction was incorrectly appended

--- src/utils/appendReaction.ts
+++ src/utils/appendReaction.ts
@@ -9,20 +9,16 @@ export default async function appendReaction(message: Message) {
     const content = message.content.split('\n')
     const reactionsData: AppendProps[] = []
     const field = { name: "", value: "" }
-    
-    // Fetch all reactions in parallel for all the reactions in the cache
+    const fields = []
     const reactionPromises = Array.from(message.reactions.cache.values()).map(async (reaction) => {
         const users = await reaction.users.fetch()
         if (users.size >= 2) {
             reactionsData.push({ reaction, users })
         }
     })
-
-    // Wait for all the fetches to complete
     await Promise.all(reactionPromises)
 
     for (const { reaction, users } of reactionsData) {
-        console.log("Continued with", reaction.emoji.name)
         const reactionContent = content.find((a) => a.includes(reaction.emoji.name || ''))
         for (const [, author] of users) {
             if (author.username === 'puzzlesoup_86746') {
@@ -31,9 +27,13 @@ export default async function appendReaction(message: Message) {
 
             field.name = `${reactionContent?.replaceAll(`/${reaction.emoji.name}/g`, `<${reaction.emoji.name}:${reaction.emoji.id}`)}\n`
             field.value = `<@${author.id}>\n`
+            if (field.name.length > 0 && field.value.length > 0) {
+                fields.push({...field})
+            }
+            field.name = ""
+            field.value = ""
         }
     }
 
-    console.log("returning", field)
-    return field
+    return fields
 }
